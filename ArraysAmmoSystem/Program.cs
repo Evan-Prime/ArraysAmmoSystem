93c0354288212f0663d4464033195eac5e11ba4c
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-10-24 11:13:41 -0300

    Initial commit

---
+++ ArraysAmmoSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAmmoSystem
+{
+    internal class Program
+    {
+
+        static int [] ammo;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" +-----------+");
+            Console.WriteLine(" |Ammo System|");
+            Console.WriteLine(" +-----------+");
+            Console.WriteLine("");
+
+            
+        }
+    }
+}
